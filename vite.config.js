a5b211f9ea7a2b6e276b9fa78dd91deeb5d2bf49
Tal Hafzadi <talhafzadi5@gmail.com>
2025-06-25 14:00:44 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -205,7 +205,6 @@ export default defineConfig({
                 'resources/js/groups/groups.ts',
                 'resources/js/groups/edit.ts',
                 'resources/js/ranks/ranks.ts',
-                'resources/js/ranks/playtime.ts',
                 'resources/js/admin/delete.ts',
                 'resources/js/nav.js',
                 'resources/js/vip/list.ts',
